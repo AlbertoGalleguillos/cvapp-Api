a51142ec2ff8304fd1fddc6b06ff09c7458fbe35
Alberto Galleguillos <ai.galleguillosr@gmail.com>
2017-10-16 21:56:59 -0300

    update jobModel

--- api/models/apiModel.js
+++ api/models/apiModel.js
@@ -2,21 +2,23 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+//Company, Lapse, Role
 var JobSchema = new Schema({
-    name: {
+    company: {
         type: String,
-        required: 'Kindly enter the name of the job'
+        required: 'Kindly enter the name of the company'
+    },
+    lapse: {
+        type: String,
+        required: 'Kindly enter the lapse working in the company'
+    },
+    role: {
+        type: String,
+        required: 'Kindly enter the role in the company'
     },
     Created_date: {
         type: Date,
         default: Date.now
-    },
-    status: {
-        type: [{
-            type: String,
-            enum: ['pending', 'ongoing', 'completed']
-        }],
-        default: ['pending']
     }
 });
 
